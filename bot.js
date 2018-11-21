b76a510d711ef84ef21b6f6f246efc7ba988e2be
MinatoUnder <44467517+MinatoUnder@users.noreply.github.com>
2018-11-21 23:13:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -229,8 +229,8 @@ function play(guild, song) {
     serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
 }
  
-const adminprefix = "$vip";
-const devs = ['274923685985386496'];
+const adminprefix = "1";
+const devs = ['415595760990552065'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
